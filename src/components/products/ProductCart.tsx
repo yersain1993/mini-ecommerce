07640996b4278cd5f2a02948c2f6614883211783
Yersain Castaño <yercas93@gmail.com>
2025-11-12 11:32:40 -0500

    localstorage with zustand´s persist

--- src/components/products/ProductCart.tsx
+++ src/components/products/ProductCart.tsx
@@ -1,44 +1,118 @@
+import React from "react";
 import { useCartStore } from "../../stores/useCartStore";
 
 const ProductCart: React.FC = () => {
+  const {
+    items,
+    isPaid,
+    increase,
+    decrease,
+    remove,
+    clear,
+    purchase,
+    resetPurchase,
+  } = useCartStore();
 
-  const { items, increase, decrease, remove, clear } = useCartStore();
-  const totalPrice = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  const totalPrice = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
 
-  console.log(totalPrice);
+  const handlePurchase = () => {
+    if (items.length === 0) return;
+    clear();
+    purchase();
+  };
+
+    const handleNewPurchase = () => {
+    resetPurchase();
+  };
+
+
   return (
-    <div className="w-full absolute z-10 p-5">
-      <section>
-        <ul className="w-full h-full flex flex-col items-center justify-center">
-            {items ? (
-                items.map((item: any) => (
-                <li key={item.id} className="flex flex-col items-center">
-                    <img src={item.images[0]} alt={item.title} className="size-28"/>
-                    <h2>{item.title}</h2>
-                    <span>${item.price}</span>
-                    <div className="w-full flex justify-evenly">
-                      <button onClick={() => increase(item.id)}>+</button>
-                      <span>{item.quantity}</span>
-                      <button onClick={() => decrease(item.id)}>-</button>
-                    </div>
-                    <span>Subtotal: ${item.price * item.quantity}</span>
-                    <button onClick={() => remove(item.id)}>Eliminar</button>
-                </li>
-                ))
-                
+    <div className="w-full h-full absolute z-10 p-5">
+      <section className="flex flex-col items-center">
+        {isPaid ? (
+          <div className="flex flex-col items-center justify-between">
+            <p className="text-2xl text-green-700 font-bold mt-10">
+            ¡Gracias por su compra!
+          </p>
+            <button
+              className="bg-sky-200 px-3 mt-10 rounded-full text-sky-900 active:bg-sky-600 active:text-sky-200 font-bold"
+              onClick={handleNewPurchase}
+            >
+              Comprar de nuevo
+            </button>
+          </div>
+        ) : (
+          <>
+            {items.length > 0 ? (
+              <>
+                <ul className="w-full flex flex-wrap items-center justify-center gap-3.5">
+                  {items.map((item) => (
+                    <li
+                      key={item.id}
+                      className="flex flex-col items-center p-3 rounded-t-2xl border border-sky-200 shadow-sm"
+                    >
+                      <img
+                        src={item.images[0]}
+                        alt={item.title}
+                        className="size-28 object-contain"
+                      />
+                      <h2 className="font-semibold">{item.title}</h2>
+                      <span>${item.price}</span>
+
+                      <div className="w-full flex justify-evenly items-center mt-1">
+                        <button
+                          className="bg-sky-200 px-3 rounded-l-full text-sky-900 active:bg-sky-600 active:text-sky-200 font-bold"
+                          onClick={() => increase(item.id)}
+                        >
+                          +
+                        </button>
+                        <span className="font-bold">{item.quantity}</span>
+                        <button
+                          className="bg-sky-200 px-3 rounded-r-full text-sky-900 active:bg-sky-600 active:text-sky-200 font-bold"
+                          onClick={() => decrease(item.id)}
+                        >
+                          -
+                        </button>
+                      </div>
+
+                      <span className="mt-1">
+                        Subtotal: ${item.price * item.quantity}
+                      </span>
+
+                      <button
+                        className="text-3xl text-sky-500 active:text-sky-50 active:scale-90 mt-2"
+                        onClick={() => remove(item.id)}
+                      >
+                        <i className="bx bx-trash-alt"></i>
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex flex-col text-center mt-5">
+                  <span className="text-lg font-semibold text-sky-900">
+                    Total: ${totalPrice}
+                  </span>
+                  <button
+                    className="bg-sky-950 px-5 py-2 mt-3 rounded-full text-sky-300 active:bg-sky-600 active:text-sky-200 font-bold"
+                    onClick={handlePurchase}
+                  >
+                    Pagar
+                  </button>
+                </div>
+              </>
             ) : (
-                <p>No hay productos en el carrito</p>
+              <p className="text-sky-500 text-3xl font-bold text-center mt-10">
+                No hay productos en el carrito
+              </p>
             )}
-        </ul>
-        <span>Total: ${totalPrice}</span>
-        <div className="flex flex-col text-center">
-          
-          <button onClick={() => clear()}>Pagar</button>
-        </div>
+          </>
+        )}
       </section>
     </div>
-    
-  )
+  );
 };
 
 export default ProductCart;
--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -1,73 +1,81 @@
 import { create } from "zustand";
-
-
-interface Category {
-    id: number;
-    name: string;
-    slug: string;
-}
+import { persist } from "zustand/middleware";
 
 export interface CartItem {
     id: number;
     title: string;
-    slug: string;
     price: number;
     description: string;
-    category: Category;
-    thumbnail: string;
     images?: string[];
     quantity: number;
 }
 
 interface CartState {
     items: CartItem[];
+    isPaid: boolean;
 
     add: (item: Omit<CartItem, "quantity">) => void; // CONSULTAR SOBRE OMIT
     remove: (id: number) => void;
     clear: () => void;
+    purchase: () => void;
+    resetPurchase: () => void;
 
     increase: (id: number) => void;
     decrease: (id: number) => void;
 }
 
-export const useCartStore = create<CartState>((set, get) => ({
-  items: [],
+export const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+    items: [],
+    isPaid: false,
 
-  add: (item) => {
-    const items = get().items;
-    const exists = items.find((i) => i.id === item.id);
+    add: (item) => {
+      const items = get().items;
+      const exists = items.find((i) => i.id === item.id);
 
-    if (exists) {
-      return set({
-        items: items.map((i) =>
-          i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+      if (exists) {
+        return set({
+          items: items.map((i) =>
+            i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+          ),
+        });
+      }
+
+      set({ items: [...items, { ...item, quantity: 1 }] });
+    },
+
+    remove: (id) =>
+      set({
+        items: get().items.filter((i) => i.id !== id),
+      }),
+
+    increase: (id) =>
+      set({
+        items: get().items.map((i) =>
+          i.id === id ? { ...i, quantity: i.quantity + 1 } : i
         ),
-      });
+      }),
+
+    decrease: (id) =>
+      set({
+        items: get().items
+          .map((i) =>
+            i.id === id ? { ...i, quantity: i.quantity - 1 } : i
+          )
+          .filter((i) => i.quantity > 0),
+      }),
+
+    clear: () => set({ items: [] }),
+    
+    purchase: () => set({isPaid: true}),
+    resetPurchase: () => set({ isPaid: false }),
+
+}),
+
+    {
+      name: "cart-storage", // 🔹 clave del localStorage
+      partialize: (state) => ({ items: state.items }), // 🔹 solo guardamos el carrito (no isPaid)
     }
-
-    set({ items: [...items, { ...item, quantity: 1 }] });
-  },
-
-  remove: (id) =>
-    set({
-      items: get().items.filter((i) => i.id !== id),
-    }),
-
-  increase: (id) =>
-    set({
-      items: get().items.map((i) =>
-        i.id === id ? { ...i, quantity: i.quantity + 1 } : i
-      ),
-    }),
-
-  decrease: (id) =>
-    set({
-      items: get().items
-        .map((i) =>
-          i.id === id ? { ...i, quantity: i.quantity - 1 } : i
-        )
-        .filter((i) => i.quantity > 0),
-    }),
-
-  clear: () => set({ items: [] }),
-}));
+  )
+  );
